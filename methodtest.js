c46589c9e6ea1f229591dfb0548aea463f12d3e6
wujoannec <81791252+wujoannec@users.noreply.github.com>
2023-03-15 23:42:36 -0700

    Small fixes

--- methodtest.js
+++ methodtest.js
@@ -13,12 +13,9 @@ export function postReq() {
 		xhr.open("POST", url, true);
 		xhr.setRequestHeader("Content-Type", "application/json");
 		xhr.onreadystatechange = () => { // Call a function when the state changes.
-			const output = document.getElementById("outputty");
-			if (xhr.readyState === XMLHttpRequest.DONE && xhr.status != 200){
-				output.innerText = "Response: "+ xhr.status;
-			}
+			const output = document.getElementById("response");
 			if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
-				output.innerText = "Response: "+ xhr.status + '\n'+ xhr.responseText;
+				output.innerText = xhr.responseText;
 			}
 		}
 		const jsonObj = JSON.stringify({"recordId": formy2Id, "articleName": formy2Name, 
@@ -38,7 +35,7 @@ export function getReq() {
 		let formData = new FormData(document.querySelector("#formy2"));			
 		let url = new URL("https://httpbin.org/get");
 		url.search = new URLSearchParams(formData);
-		const output = document.getElementById("outputty");
+		const output = document.getElementById("response");
 		fetch(url, {
 			method: 'GET',
 	})
@@ -53,6 +50,7 @@ export function putReq() {
 	const putButton = document.getElementById("putButton");
 	putButton.addEventListener("click", (event) => {
 			event.preventDefault();
+			document.getElementById("todays-date").value = new Date();
 			const formy2Id = document.querySelector("#record-id").value;
 			const formy2Name = document.querySelector("#article-name").value;
 			const formy2Body = document.querySelector("#article-body").value;
@@ -60,7 +58,7 @@ export function putReq() {
 			const jsonObj = JSON.stringify({"articleName": formy2Name, 
 				"articleBody": formy2Body, "todaysDate": formy2Date});
 			let url = "https://httpbin.org/put";
-			const output = document.getElementById("outputty");
+			const output = document.getElementById("response");
 			fetch(url, {
 				method: 'PUT',
 				body: jsonObj
@@ -76,7 +74,7 @@ export function deleteReq() {
 	const deleteButton = document.getElementById("deleteButton");
 	deleteButton.addEventListener("click", (event) => {
 			event.preventDefault();
-			console.log("ey");
+			document.getElementById("todays-date").value = new Date();
 			const formy2Name = document.querySelector("#article-name").value;
 			const formy2Body = document.querySelector("#article-body").value;
 			const formy2Date = document.querySelector("#todays-date").value; 
@@ -91,7 +89,7 @@ export function deleteReq() {
 			})
 			.then(response => response.json())
 			.then(data => {
-				const output = document.getElementById("outputty");
+				const output = document.getElementById("response");
 				output.innerText = JSON.stringify(data, null, 2);
 			})
 		});
